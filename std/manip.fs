7a64a3680954007970e48205458778dcbdaad56f
Robyn <wrenwreu@gmail.com>
2024-02-13 14:27:41 -0500

    Added RFETCH ABS.

--- std/manip.fs
+++ std/manip.fs
@@ -7,6 +7,8 @@
 : PICK ( x_u ... x_1 x_0 u -- x_u ... x_1 x_0 x_u )
 	1+ 8 * DSP@ + @ ;
 
+: R@ ( -- x ) ( R: x -- x ) R> R> TUCK >R >R ;
+
 : MIN ( n1 n2 -- n3 )
 	2DUP > IF
 		SWAP
@@ -16,3 +18,8 @@
 	2DUP < IF
 		SWAP
 	THEN DROP ;
+
+: ABS ( n -- +n )
+	DUP 0< IF
+		NEGATE
+	THEN ;
