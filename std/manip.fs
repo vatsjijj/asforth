ea4679d82c34cab658ec4655a2889c8cf7704190
Robyn <wrenwreu@gmail.com>
2024-02-13 13:22:27 -0500

    Added MIN MAX.

--- std/manip.fs
+++ std/manip.fs
@@ -6,3 +6,13 @@
 
 : PICK ( x_u ... x_1 x_0 u -- x_u ... x_1 x_0 x_u )
 	1+ 8 * DSP@ + @ ;
+
+: MIN ( n1 n2 -- n3 )
+	2DUP > IF
+		SWAP
+	THEN DROP ;
+
+: MAX ( n1 n2 -- n3 )
+	2DUP < IF
+		SWAP
+	THEN DROP ;
